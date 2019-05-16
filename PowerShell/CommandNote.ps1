0a2effa7e84b671c34a69754282165e9b04a35c2
KMKZ_Yozora <kamikazelight@gmail.com>
2019-05-16 15:26:49 +0900

    powershell commandNote 追加

---
+++ PowerShell/CommandNote.ps1
@@ -0,0 +1,28 @@
+﻿# ハードウェア制御
+    # シャットダウン
+    Stop-Computer
+
+    # 再起動
+    Restart-Computer
+
+# エクスプローラ, Regedit
+    # 新規, フォルダ, ファイル, レジストリ
+    New-Item
+
+    # テキスト, 保存
+    Out-File
+
+    # 取得
+    Get-Item
+    Get-ChildItem
+
+    # 変更
+    # ファイルの移動
+    Move-Item
+
+    # ファイル名の変更
+    Rename-Item
+
+# オブジェクト
+    # 取得
+    New-Object -ComObject
