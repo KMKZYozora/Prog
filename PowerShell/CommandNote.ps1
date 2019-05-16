a955397a99df13473721e0d98685719978600636
KMKZ_Yozora <kamikazelight@gmail.com>
2019-05-16 16:04:18 +0900

    書き方をオブジェクト別に変更

--- PowerShell/CommandNote.ps1
+++ PowerShell/CommandNote.ps1
@@ -1,16 +1,39 @@
 ﻿# ハードウェア制御
     # シャットダウン
-    Stop-Computer
+    Stop-Computer -Force
 
     # 再起動
-    Restart-Computer
+    Restart-Computer -Force
 
-# エクスプローラ, Regedit
-    # 新規, フォルダ, ファイル, レジストリ
-    New-Item
+# エクスプローラ
+    # フォルダ
+        $Path3 = "D:\Users\Desktop/test"
+        $Path1 = "$Path3/Folder"
+        $Path2 = "$Path1`2"
+        # 生成
+            New-Item -Path $Path1 -ItemType Directory -Force
+        # 名前変更
+            Rename-Item -LiteralPath $Path1 -NewName $Path2
+        # 移動, リネーム
+            Move-Item　-LiteralPath $Path2 -Destination $Path1
+        # プロパティ 取得
+            $Time = (Get-ItemProperty -LiteralPath $Path1).LastWriteTime
+        # プロパティ 設定
+            Set-ItemProperty -Path $Path1
+        # 削除
+            Remove-Item -LiteralPath $Path3 -Recurse
 
-    # テキスト, 保存
-    Out-File
+    # テキストファイル
+        # 生成
+            New-Item -Path $Path1 -ItemType Directory -Force
+            Out-File
+            # 名前変更
+                Rename-Item -LiteralPath $Path1 -NewName $Path2
+            # 移動, リネーム
+                Move-Item　-LiteralPath $Path2 -Destination $Path1
+            # 削除
+                Remove-Item -LiteralPath $Path3 -Recurse
+    
 
     # 取得
     Get-Item
